23ccf36e2415defc63212cb2671bae28436ce765
Jérémy <jeremy.perrouault@ascadis.fr>
2025-09-26 07:29:08 +0000

    JOUR 2 - INTRO SPRING DATA JPA

--- JAVA/intro-spring-boot/src/main/java/fr/formation/repository/ProduitRepository.java
+++ JAVA/intro-spring-boot/src/main/java/fr/formation/repository/ProduitRepository.java
@@ -1,9 +1,21 @@
 package fr.formation.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.formation.model.Produit;
 
 public interface ProduitRepository extends JpaRepository<Produit, String> {
-    
+    public boolean existsByName(String name);
+    public Optional<Produit> findByName(String name);
+    public List<Produit> findAllByNameContains(String name);
+
+    // @Query("select p from Produit p where p.name = :leParamName")
+    // public Optional<Produit> findByQueryName(@Param("leParamName") String name);
+
+    @Query("select p from Produit p where p.name = ?1 order by p.id asc")
+    public Optional<Produit> findByQueryName(String name);
 }
