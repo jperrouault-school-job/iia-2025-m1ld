675336aa259dd62269f528e3d175b76ab2023ec4
Jérémy <jeremy.perrouault@ascadis.fr>
2025-10-21 08:59:16 +0000

    JOUR 4 - CORRECTION EXO

--- JAVA/spring-test/src/test/java/fr/formation/repository/ProduitRepositoryTest.java
+++ JAVA/spring-test/src/test/java/fr/formation/repository/ProduitRepositoryTest.java
@@ -1,9 +1,13 @@
 package fr.formation.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -27,5 +31,55 @@ public class ProduitRepositoryTest {
 
         // then
         Assertions.assertEquals(3, result.size());
+    }
+
+    @Test
+    void shouldFindByQueryNameReturnProduit() {
+        // given
+
+        // when
+        Optional<Produit> optProduit = this.repository.findByQueryName("Parachute");
+
+        // then
+        Assertions.assertTrue(optProduit.isPresent());
+    }
+
+    @Test
+    void shouldFindByQueryNameDontReturnProduit() {
+        // given
+
+        // when
+        Optional<Produit> optProduit = this.repository.findByQueryName("Nom qui existe pas");
+
+        // then
+        Assertions.assertFalse(optProduit.isPresent());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "Parachute,true",
+        "Autre,false"
+    })
+    void shouldFindByQueryOk(String nom, boolean isPresent) {
+        // given
+
+        // when
+        Optional<Produit> optProduit = this.repository.findByQueryName(nom);
+
+        // then
+        Assertions.assertEquals(isPresent, optProduit.isPresent());
+    }
+
+    @Test
+    void shouldFindByPriceBetweenOk() {
+        // given
+
+        // when
+        List<Produit> result = this.repository
+            .findAllByPriceBetween(new BigDecimal("400"), new BigDecimal("600"))
+        ;
+
+        // then
+        Assertions.assertEquals(1, result.size());
     }
 }
